df3b2faf17d98773932ef65a65a8322be6ece823
Reza Mardani <130505071+rezamardaniDev@users.noreply.github.com>
2024-05-01 01:24:32 +0330

    Update main.py

--- main.py
+++ main.py
@@ -6,13 +6,13 @@
 #                Screen Capture                   # 
 ###################################################
 
-
 import socketserver
 import threading
 import pyautogui
 import io
 import socket
 import tkinter as tk
+from tkinter import font
 import os
 
 class ScreenCapture(socketserver.BaseRequestHandler):
@@ -69,18 +69,24 @@ httpServer = None
 def start_server():
     global httpServer
     httpServer = socketserver.ThreadingTCPServer((HOST, PORT), ScreenCapture)
-    label.config(text=f"status: HTTP server ON\nUser can connect by\n {get_private_ip()}:{8000}")
+    label.config(text=
+f"""
+Status: HTTP server ON
+
+Users can connect through the following IP: {get_private_ip()}:{8000}
+
+Please do not close the window!
+""")
     httpServer.serve_forever()
 
 def start_function():
     global httpServer
-    if not httpServer:
+    if not httpServer:  # Change here
         ScreenCapture.is_running = True
         thread = threading.Thread(target=start_server)
         thread.daemon = True
         thread.start()
-        httpServer = True
-        
+        httpServer = True  # Change here
 
 def stop_function():
     global httpServer
@@ -89,18 +95,20 @@ def stop_function():
     if httpServer:
         httpServer.shutdown()
         httpServer.server_close()
-        httpServer = False
+        httpServer = None  # Change here
 
 root = tk.Tk()
+root.configure()
 root['pady'] = 100
 root.title("SCREEN CAPTURE")
-root.geometry("300x500")
-root.resizable(False, False)
+root.geometry("480x500")
+root.resizable(True, True)
+label_font = font.Font(weight="bold")
 
-start_button = tk.Button(root, text="Start", command=start_function, width=20, height=5)
+start_button = tk.Button(root, text="Start", command=start_function, width=15,bg='#10AC84',fg='white', height=5, font=label_font)
 start_button.pack()
 
-stop_button = tk.Button(root, text="Stop", command=stop_function, width=20, height=5)
+stop_button = tk.Button(root, text="Stop", command=stop_function, width=15,bg='#EE5253',fg='white', height=5, font=label_font)
 stop_button.pack()
 
 label = tk.Label(root, text="")
